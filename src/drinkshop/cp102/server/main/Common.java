3e1f22055c727f6d4f5277c0359d7c0aea289190
Nick Wen <y2ka.tw@gmail.com>
2018-09-07 06:48:39 +0800

    test commit

--- src/drinkshop/cp102/server/main/Common.java
+++ src/drinkshop/cp102/server/main/Common.java
@@ -1,6 +1,6 @@
 package drinkshop.cp102.server.main;
 
-public class Common {
+public class Common {//test
 	public final static String URL = "jdbc:mysql://localhost:3306/drinkshop";
 	public final static String USER = "root";
 	public final static String PASSWORD = "root";
