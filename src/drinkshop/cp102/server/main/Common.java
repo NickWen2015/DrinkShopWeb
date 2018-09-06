4cdb576659787ac843064610f0462303d08664f4
Nick Wen <y2ka.tw@gmail.com>
2018-09-07 06:53:07 +0800

    test

--- src/drinkshop/cp102/server/main/Common.java
+++ src/drinkshop/cp102/server/main/Common.java
@@ -1,6 +1,6 @@
 package drinkshop.cp102.server.main;
 
-public class Common {//test
+public class Common {
 	public final static String URL = "jdbc:mysql://localhost:3306/drinkshop";
 	public final static String USER = "root";
 	public final static String PASSWORD = "root";
