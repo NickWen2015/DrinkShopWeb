e8cbf481296656ee83d60b13bb63c72c1c8020fe
yovelachou <yovela8888@gmail.com>
2018-09-25 19:53:10 +0800

    NewsServlet 衝突已解決

--- src/drinkshop/cp102/server/news/NewsServlet.java
+++ src/drinkshop/cp102/server/news/NewsServlet.java
@@ -19,8 +19,6 @@ import com.google.gson.JsonObject;
 import drinkshop.cp102.server.main.LogHelper;
 import drinkshop.cp102.server.main.ImageUtil;
 
-
-
 /**
  * Servlet implementation class NewsServlet
  */
@@ -29,7 +27,7 @@ import drinkshop.cp102.server.main.ImageUtil;
 public class NewsServlet extends HttpServlet {
 	private final static String CONTENT_TYPE = "text/html; charset=UTF-8";
 	private NewsDao newsDao = null;
-       
+
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		if (newsDao == null) {
 			newsDao = new NewsDaoMySqlImpl();
@@ -37,7 +35,7 @@ public class NewsServlet extends HttpServlet {
 		List<News> myNews = newsDao.getAllNews();
 		writeText(response, new Gson().toJson(myNews));
 	}
-	
+
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
 		Gson gson = new Gson();
@@ -48,65 +46,68 @@ public class NewsServlet extends HttpServlet {
 			jsonIn.append(line);
 		}
 		LogHelper.showInput(jsonIn);
-		
+
 		JsonObject jsonObject = gson.fromJson(jsonIn.toString(), JsonObject.class);
 		if (newsDao == null) {
 			newsDao = new NewsDaoMySqlImpl();
 		}
-		
+
 		String action = jsonObject.get("action").getAsString();
 
-		if(action.equals("getNews_activity_id")) {
+		if (action.equals("getNews_activity_id")) {
 			List<News> mynews = newsDao.getNews_activity_id();
-			writeText(response, gson.toJson(mynews)); 
-			
-		} else if(action.equals("getNewsImage")) {
+			writeText(response, gson.toJson(mynews));
+
+		} else if (action.equals("getNewsImage")) {
 			int activity_id = jsonObject.get("activity_id").getAsInt();
 			int imageSize = jsonObject.get("imageSize").getAsInt();
 			OutputStream os = response.getOutputStream();
 			byte[] image = newsDao.getImage(activity_id);
-			if (image != null) {  
-				image = ImageUtil.shrink(image, imageSize); //縮圖
-				response.setContentType("image/jpeg"); 
-				response.setContentLength(image.length); //圖的長度
+			if (image != null) {
+				image = ImageUtil.shrink(image, imageSize); // 縮圖
+				response.setContentType("image/jpeg");
+				response.setContentLength(image.length); // 圖的長度
 			}
-			os.write(image); //輸出
-			
-		} else if (action.equals("getAllNews")) {   //拿到資料轉成JSON Object
+			os.write(image); // 輸出
+
+		} else if (action.equals("getAllNews")) { // 拿到資料轉成JSON Object
 			List<News> mynews = newsDao.getAllNews();
-			writeText(response, gson.toJson(mynews)); 
-			
+
+			writeText(response, gson.toJson(mynews));
+
 		} else if (action.equals("getImage")) {
 			OutputStream os = response.getOutputStream();
 			int id = jsonObject.get("id").getAsInt();
 			int imageSize = jsonObject.get("imageSize").getAsInt();
-			byte[] image = newsDao.getImage(id); 
-			if (image != null) {  
-				image = ImageUtil.shrink(image, imageSize); //縮圖
-				response.setContentType("image/jpeg"); 
-				response.setContentLength(image.length); //圖的長度
+			byte[] image = newsDao.getImage(id);
+			if (image != null) {
+				image = ImageUtil.shrink(image, imageSize); // 縮圖
+				response.setContentType("image/jpeg");
+				response.setContentLength(image.length); // 圖的長度
 			}
-			os.write(image); //輸出
-			
-		} else if (action.equals("newsInsert") || action.equals("newsUpdate")) {  
-			String newsJson = jsonObject.get("news").getAsString();  //JSON字串裡面有JSON字串
-			News news = gson.fromJson(newsJson, News.class);  //解析成news物件
+			os.write(image); // 輸出
+
+		} else if (action.equals("newsInsert") || action.equals("newsUpdate")) {
+			String newsJson = jsonObject.get("news").getAsString(); // JSON字串裡面有JSON字串
+			News news = gson.fromJson(newsJson, News.class); // 解析成news物件
 			String imageBase64 = jsonObject.get("imageBase64").getAsString();
 			System.out.println("imageBase64 = " + imageBase64);
 			byte[] image = Base64.getMimeDecoder().decode(imageBase64);
 			;
 			int count = 0;
-			if (action.equals("newsInsert")) {  
+			if (action.equals("newsInsert")) {
 				count = newsDao.insert(news, image);
 			} else if (action.equals("newsUpdate")) {
 				count = newsDao.update(news, image);
 			}
 			writeText(response, String.valueOf(count));
-		}  else if (action.equals("newsDelete")) {
+
+		} else if (action.equals("newsDelete")) {
 			String newsJson = jsonObject.get("news").getAsString();
 			News news = gson.fromJson(newsJson, News.class);
-			int count = newsDao.delete(news.getActivity_id()); 
+			int count = newsDao.delete(news.getActivity_id());
 			writeText(response, String.valueOf(count));
+
 		} else if (action.equals("findById")) {
 			int id = jsonObject.get("id").getAsInt();
 			News news = newsDao.findById(id);
@@ -115,14 +116,12 @@ public class NewsServlet extends HttpServlet {
 			writeText(response, "");
 		}
 	}
-	
-	
-	
-private void writeText(HttpServletResponse response, String outText) throws IOException {
-	response.setContentType(CONTENT_TYPE); //純文字
-	PrintWriter out = response.getWriter();
-	out.print(outText);
-	LogHelper.showOutput(outText);
+
+	private void writeText(HttpServletResponse response, String outText) throws IOException {
+		response.setContentType(CONTENT_TYPE); // 純文字
+		PrintWriter out = response.getWriter();
+		out.print(outText);
+		LogHelper.showOutput(outText);
 	}
 
 }
